a281ee0161d4f2cd5a14849b7d7cae69c124ba3e
Muhammad-Ali <jafarimuhammadali36@gmail.com>
2019-08-22 15:16:05 +0430

    Some Changes Were Made

--- src/layout/SignedInLink.js
+++ src/layout/SignedInLink.js
@@ -6,14 +6,14 @@ function SignedInLinks() {
     <div>
       <ul className="right">
         <li>
-          <NavLink to="/">New Projects</NavLink>
+          <NavLink to="/create">New Project</NavLink>
         </li>
         <li>
           <NavLink to="/">Log Out</NavLink>
         </li>
         <li>
           <NavLink to="/" className="btn btn-floating pink lighten-1">
-            NN
+            JcS
           </NavLink>
         </li>
       </ul>
